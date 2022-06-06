57c38667647dd3ba2b0cc03a318feb620a5126a8
Nikhil Thadani <nikhilthadani112256@gmail.com>
2022-06-06 22:32:06 +0530

    added seos

--- indiancoders/pages/index.js
+++ indiancoders/pages/index.js
@@ -1,5 +1,17 @@
 import MidPage from "../components/MidPage";
+import Head from "next/head";
 
 export default function Home() {
-  return <MidPage />;
+  return (
+    <>
+      <Head>
+        <title>Indian Coders: Platform For In Depth Coding Courses</title>{" "}
+        <meta
+          name="description"
+          content="Welcome to Indian Coders, Platform to Learn Coding and Programming with in depth courses"
+        />
+      </Head>
+      <MidPage />
+    </>
+  );
 }
